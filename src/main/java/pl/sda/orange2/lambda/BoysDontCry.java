00acc1f78a01773842456a9c6e1db237ebd101d1
lucyna <lunda@onet.pl>
2023-03-04 12:38:10 +0100

    [add] - lambda exercises started

---
+++ src/main/java/pl/sda/orange2/lambda/BoysDontCry.java
@@ -0,0 +1,6 @@
+package pl.sda.orange2.lambda;
+
+public interface BoysDontCry {
+
+    void silnoreki();
+}
---
+++ src/main/java/pl/sda/orange2/lambda/LambdaExercises.java
@@ -0,0 +1,7 @@
+package pl.sda.orange2.lambda;
+
+public class LambdaExercises {
+    public static void main(String[] args) {
+
+    }
+}
